82090d2e2afa175a21ffc05f01ecfeccf074c0a5
‘AMAYA <aamaya547@gmail.com>
2022-08-30 10:40:20 -0700

    get all cards failing with a 404 as expected

---
+++ __tests__/cards.test.js
@@ -0,0 +1,18 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('cards backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
+
+it(' #GET all should return a list cards', async () => {
+  const res = await request(app).get('/api/v1/cards');
+  expect(res.status).toBe(200);
+});
