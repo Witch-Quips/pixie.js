714fe02c19a5aaeea11d8fb7baaf9be72803f3c6
‘AMAYA <aamaya547@gmail.com>
2022-08-31 14:53:04 -0700

    updated test and model name to be meanings, wrote Meaning model

--- __tests__/meanings.test.js
+++ __tests__/meanings.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('Descriptions backend-express-template routes', () => {
+describe('Meanings backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
